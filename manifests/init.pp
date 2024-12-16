53d8a8e3d4d2734b3af173239243e7dbe99b30ac
bfrye-sans <48800510+bfrye-sans@users.noreply.github.com>
2024-12-16 17:00:29 -0500

    update

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,24 +4,26 @@ class tenable (
     agent_version        => $tenable::params::agent_version,
     agent_port           => $tenable::params::agent_port,
     agent_key            => $tenable::params::agent_key,
-    service_enable       => $tenable::params::service_enable,
-    service_ensure       => $tenable::params::service_ensure,
-    proxy_host           => $tenable::params::proxy_host,
-    proxy_port           => $tenable::params::proxy_port,
-    host                 => $tenable::params::host,
-    cloud                => $tenable::params::cloud,
-    group                => $tenable::params::group,
+    agent_source         => $tenable::params::agent_source,
+    agent_package        => $tenable::params::agent_package,
+    agent_service_enable => $tenable::params::agent_service_enable,
+    agent_service_ensure => $tenable::params::agent_service_ensure,
+    agent_proxy_host     => $tenable::params::agent_proxy_host,
+    agent_proxy_port     => $tenable::params::agent_proxy_port,
+    agent_host           => $tenable::params::agent_host,
+    agent_cloud          => $tenable::params::agent_cloud,
+    agent_group          => $tenable::params::agent_group,
   }
 
   class { 'tenable::security_center':
     security_center_version => $tenable::params::security_center_version,
-    service_enable => $tenable::params::service_enable,
-    service_ensure => $tenable::params::service_ensure,
+    service_enable => $tenable::params::security_center_service_enable,
+    service_ensure => $tenable::params::security_center_service_ensure,
   }
 
   class { 'tenable::scanner':
     scanner_version => $tenable::params::scanner_version,
-    service_enable => $tenable::params::service_enable,
-    service_ensure => $tenable::params::service_ensure,
+    service_enable => $tenable::params::scanner_service_enable,
+    service_ensure => $tenable::params::scanner_service_ensure,
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,18 @@
 class tenable::params {
-  # Shared Params
-  $service_ensure = 'running'
-  $service_enable = true
-
-  # Agent Params
   $agent_version = 'latest'
   $agent_port = 8834
-  $agent_key = 'your-activation-key'
-  $proxy_host = undef
-  $proxy_port = undef
-  $host = undef
-  $cloud = true
-  $group = undef
-  }
-
-  # Scanner Params
-  $scanner_version = 'latest'
-
-  # Security Center Params
+  $agent_key = undef
+  $agent_service_enable = true
+  $agent_service_ensure = 'running'
+  $agent_proxy_host = undef
+  $agent_proxy_port = undef
+  $agent_host = undef
+  $agent_cloud = false
+  $agent_group = undef
   $security_center_version = 'latest'
+  $security_center_service_enable = true
+  $security_center_service_ensure = 'running'
+  $scanner_version = 'latest'
+  $scanner_service_enable = true
+  $scanner_service_ensure = 'running'
 }
