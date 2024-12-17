830baf5ba88871400ad0b1f20c56342a2eb2e2b2
bfrye-sans <48800510+bfrye-sans@users.noreply.github.com>
2024-12-17 11:59:54 -0500

    Update

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -57,7 +57,7 @@ class tenable::agent (
 
   # Populate the Nessus version fact file conditionally
   exec { 'get_nessus_agent_version':
-    command   => '/bin/bash -c "if command -v /opt/nessus_agent/sbin/nessuscli > /dev/null 2>&1; then /opt/nessus_agent/sbin/nessuscli -v | sed -n \"s/.*Nessus Agent) \\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/\\1/p\" > /opt/puppetlabs/facter/facts.d/nessus_version.txt; else echo \"nessus_version=0.0.0\" > /opt/puppetlabs/facter/facts.d/nessus_version.txt; fi"',
+    command   => '/bin/bash -c "if command -v /opt/nessus_agent/sbin/nessuscli > /dev/null 2>&1; then /opt/nessus_agent/sbin/nessuscli -v | sed -n \"s/.*Nessus Agent) \\([0-9]\\+\\.[0-9]\\+\\.[0-9]\\+\\).*/nessus_version=\\1/p\" > /opt/puppetlabs/facter/facts.d/nessus_version.txt; else echo \"nessus_version=0.0.0\" > /opt/puppetlabs/facter/facts.d/nessus_version.txt; fi"',
     unless    => '/usr/bin/test -f /opt/puppetlabs/facter/facts.d/nessus_version.txt',
     path      => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
     logoutput => true,
